27cc3887bad29df052d454cfc644944b9a2e1349
holabayor <liasu.olabayor@gmail.com>
2023-02-01 19:44:51 +0100

    pycodesyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ def filter_datum(fields: List[str], redaction: str,
     ''' Function that returns the log message obfuscated '''
     for field in fields:
         log_message = re.sub(rf"{field}=(.*?)\{separator}",
-                        f'{field}={redaction}{separator}', message)
+                             f'{field}={redaction}{separator}', message)
     return log_message
 
 
