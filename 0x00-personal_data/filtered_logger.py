fcacd2e90ff57e347832d5f64603cadd57f6c0d9
holabayor <liasu.olabayor@gmail.com>
2023-02-01 20:05:52 +0100

    naming convention fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     ''' Function that returns the log message obfuscated '''
     for field in fields:
-        log_message = re.sub(f'{field}=.*?{separator}',
-                             f'{field}={redaction}{separator}', message)
-    return log_message
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
