ae36e09d411d8eeb5b60150529b90c8309c3311b
holabayor <liasu.olabayor@gmail.com>
2023-02-02 13:22:54 +0100

    task 1 doc

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -25,10 +25,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        ''' Logging constructor'''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        ''' Filters values in incoming log records using filter_datum '''
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
