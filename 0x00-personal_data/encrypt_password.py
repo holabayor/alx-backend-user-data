8a23d2342a62deab43ea3df10a44454d7cad4b10
holabayor <liasu.olabayor@gmail.com>
2023-02-02 17:31:37 +0100

    check hashed *****

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,5 +9,11 @@ def hash_password(password: str) -> bytes:
     ''' function that expects one string argument name password
         Returns a salted, hashed password, which is a byte string.
     '''
-    pwd = password.encode('utf-8')
-    return bcrypt.hashpw(pwd, bcrypt.gensalt())
+    password = password.encode('utf-8')
+    return bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' function that expects 2 arguments and returns a boolean. '''
+    password = password.encode('utf-8')
+    return bcrypt.checkpw(password, hashed_password)
