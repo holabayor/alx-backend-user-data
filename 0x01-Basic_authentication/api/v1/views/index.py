47eedeb3253eaf85d333a2a3869243cb45cb9375
holabayor <liasu.olabayor@gmail.com>
2023-02-06 22:38:11 +0100

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized():
       - raise a 401 error by using abort
     '''
     abort(401)
+
+
+@app_views.route('forbidden', strict_slashes=False)
+def unauthorized():
+    ''' GET /api/v1/forbidden
+    Return:
+      - raise a 403 error by using abort
+    '''
+    abort(403)
