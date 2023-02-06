f3d8945ae21e9bcb1ca3db8d4c04e62e38280eb7
holabayor <liasu.olabayor@gmail.com>
2023-02-06 22:51:20 +0100

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module of Authentication
+"""
+from flask import request
+from typing import List
+
+
+class Auth:
+    '''
+        API class
+    '''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+            require auth
+        '''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''
+            Auth header
+        '''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+            Current User
+        '''
+        return None
