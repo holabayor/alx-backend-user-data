a502afdd50d5012225809041257a2a6ad3f7086a
holabayor <liasu.olabayor@gmail.com>
2023-02-07 21:56:48 +0100

    advanced task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,7 +53,7 @@ class BasicAuth(Auth):
                 not isinstance(decoded_base64_authorization_header, str) or\
                 ':' not in decoded_base64_authorization_header:
             return None, None
-        return tuple(decoded_base64_authorization_header.split(':'))
+        return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
@@ -70,5 +70,22 @@ class BasicAuth(Auth):
             for user in users:
                 if user.is_valid_password(user_pwd):
                     return user
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' overloads Auth and retrieves the User instance for a request
+        Returns:
+            User instance for a request
+        '''
+        try:
+            auth_header = self.authorization_header(request)
+            base64_auth_header = self.extract_base64_authorization_header(
+                auth_header)
+            decoded_auth_header = self.decode_base64_authorization_header(
+                base64_auth_header)
+            user, pwd = self.extract_user_credentials(
+                decoded_auth_header)
+            return self.user_object_from_credentials(user, pwd)
         except Exception:
             return None
