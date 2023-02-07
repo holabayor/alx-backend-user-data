f88da0c43696afb8514cd5dd77eafc7ef1c5a665
holabayor <liasu.olabayor@gmail.com>
2023-02-07 20:59:57 +0100

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Authentication
 """
+from base64 import b64decode
+from typing import Tuple
 from .auth import Auth
 
 
@@ -21,3 +23,33 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str)\
+            -> str:
+        ''' Base64 part method
+        Return:
+            - decoded value of a Base64 string base64_authorization_header
+        '''
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = b64decode(encoded)
+            return decoded64.decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str)\
+            -> Tuple[str, str]:
+        '''
+        Return:
+            the user email and password from the Base64 decoded value
+        '''
+        if decoded_base64_authorization_header is None or\
+                not isinstance(decoded_base64_authorization_header, str) or\
+                ':' not in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':'))
