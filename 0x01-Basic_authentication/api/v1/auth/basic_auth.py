27416fea7430a9027a0dec7e052f3efbcf1088d0
holabayor <liasu.olabayor@gmail.com>
2023-02-07 17:25:54 +0100

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,10 @@ class BasicAuth(Auth):
     '''
         API class
     '''
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
