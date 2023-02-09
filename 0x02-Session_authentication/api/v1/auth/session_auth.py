464a27728a4d4dde6aef8c4fb4fef6ecb82ee89e
holabayor <liasu.olabayo@gmail.com>
2023-02-09 22:06:24 +0100

    logout session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -44,3 +44,18 @@ class SessionAuth(Auth):
             return None
 
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        '''
+        deletes the user session / logout
+        '''
+        if request is None:
+            return False
+        if self.session_cookie(request) is None:
+            return False
+        session_id = self.session_cookie(request)
+        if self.user_id_for_session_id:
+            return False
+        sess = self.user_id_by_session_id(session_id)
+        del sess
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -27,9 +27,24 @@ def authenticate_session() -> str:
     for user in users:
         if user.is_valid_password(user_pwd):
             from api.v1.app import auth
+
             session_id = auth.create_session(user.id)
             SESSION_NAME = getenv('SESSION_NAME')
             response = jsonify(user.to_json())
             response.set_cookie(SESSION_NAME, session_id)
             return response
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout/', methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """ GET /auth_session/logout
+    Return:
+      - deletes the user session / logout
+    """
+    from api.v1.app import auth
+
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
