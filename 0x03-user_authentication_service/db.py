a4535a7ff7443766d33242b700b88e9becb74520
holabayor <liasu.olabayo@gmail.com>
2023-02-14 07:20:52 +0100

    annotations

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user
         """
         user = User(email=email, hashed_password=hashed_password)
