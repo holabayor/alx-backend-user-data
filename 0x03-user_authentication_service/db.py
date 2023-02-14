dfaadb9f5399aacbd99168dc3977a520e77e2479
holabayor <liasu.olabayo@gmail.com>
2023-02-14 07:28:35 +0100

    tired commit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
+from xmlrpc.client import FastMarshaller
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -18,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db")  # , echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
