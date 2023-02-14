69aeeb34bfd41f243aa57218ce14d30b53ebbd1d
holabayor <liasu.olabayo@gmail.com>
2023-02-14 11:59:06 +0100

    logout method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,10 +60,13 @@ def logout():
     Delete the session cookie and redirect
     '''
     session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect('/')
+        else:
+            abort(403)
     else:
         abort(403)
 
